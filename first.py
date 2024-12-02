613d2bbb6b7da3f1d10eb9f69a15e32415c3f36c
Step <ctep0203@mail.ru>
2024-12-02 21:44:07 +0400

    done

---
+++ first.py
@@ -0,0 +1,7 @@
+def print_hi(name):
+    print("My first git-repo")  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
